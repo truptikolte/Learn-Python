d8cee4a951173457bbc9ac8d2378da5a01c36a0e
trupti <tnkolte@gmail.com>
2020-04-01 09:00:25 +0100

    Added pipfiles

---
+++ main.py
@@ -0,0 +1,14 @@
+# print any any string
+print('hello')
+#String Literals
+#Literals are constants used in python
+#literal are data given variables as constant
+#single literals
+name = "Namah Shivay"
+print(name)
+#multiple line print
+mulitiple = '''OM
+Hrom 
+Jum
+Sa'''
+print(mulitiple)
