d957398b3eb20eb1c93c612f0ffbdf71085c6835
trupti <tnkolte@gmail.com>
2020-04-01 18:33:11 +0100

    Added pipfiles

---
+++ datatype.py
@@ -0,0 +1,8 @@
+str = 'Good Morning' # string data type
+num = 123 #integer data type
+fnum = 3.14 # float data type
+print(str,num,fnum)
+print(type(str))
+print(type(num))
+print(type(fnum)) #  type function return the data type of any variable
+
