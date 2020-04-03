08ff2f6ecc738b9180552e8e91390803dca5f0e6
trupti <tnkolte@gmail.com>
2020-04-03 11:14:37 +0100

    Added pipfiles

---
+++ mytuple.py
@@ -0,0 +1,9 @@
+mytuple1 = ('apple','banana','orange','kiwi')
+mytuple2 = ('okara','spinach','cabbage')
+print('Tuple1 : ',mytuple1)
+print('Tuple2 : ',mytuple2)
+#concatanation of tuples
+mytuple1 += mytuple2
+print('Tuple after concatanation : ', mytuple1)
+mytuple1 = mytuple1 * 2
+print('Double the list :',mytuple1)
