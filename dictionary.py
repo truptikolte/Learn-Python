6289f47db14e853e6eaf2a7a3c26f252313e8b51
trupti <tnkolte@gmail.com>
2020-04-04 10:36:30 +0100

    Added pipfiles

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+mydict = { 1 :'red', 2:'blue', 3:'yellow', 4:'orange', 5:'green'}
+print(mydict)
+# mixed key dictionary
+mydict1={'name': 'Ayushi', 'age':17, 'address' : '91 crofton Road'}
+print(mydict1)
+mydict2={'name': 'sakshi',2:[10,23]}
+print(mydict2)
+#pair dictionary
+mydict3={(2,'Nitin'),(1,'Swati'),(3,'Megha')}
+print(mydict3)
+print(mydict[1])
+print(mydict1['name'])
+#print dictionary keys
+print(mydict1.keys())
+#print values from dictionary
+print(mydict1.values())
+print(mydict1.items())
+#adding items in dictionary
+mydict1['Hobby'] = 'Dance'
+print(mydict1)
+  #length of the dictionary
+print('Length of dictionary 1 : ',len(mydict1))
+
