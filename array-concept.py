51e4d5e54ef8721ef4ad885ec5079994fbce25b4
trupti <tnkolte@gmail.com>
2020-04-09 15:01:15 +0100

    concept of array

---
+++ array-concept.py
@@ -0,0 +1,15 @@
+# array is same as list but only collection of same data type
+# array is memory efficient but slower than lists
+# declare the array(always mention the data type)
+import array as arr
+a=arr.array('i',[2,4,6,8,9])
+print(a)
+
+from array import *
+b= array('I',[5,6,7])
+print(b)
+
+c=array('f',[2.01,2.44,5.33])
+print(c)
+
+#char=array('B',['a','b','c'])
